0b8549f454db59e17a70871448678b0ca872c28c
Mason Balbera <mhbalbera@gmail.com>
2019-08-06 15:12:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,16 @@
 class Shoe 
   
   attr_accessor :brand, :color, :size, :material, :condition
+  
   def initialize(brand, color=nil, size=nil, material=nil, condition=nil)
     @brand = brand
     @color=color
     @size=size
     @material=material
     @condition=condition
+  end
+  
+  def cobbble
   end
   
 end 
