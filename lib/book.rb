30385cf00b2fcf637ebe485021b51c909b5053fa
Mason Balbera <mhbalbera@gmail.com>
2019-08-06 15:09:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,17 @@
 
 
 class Book 
-  attr_accessor :author, :page_count, :genre, :title
+  attr_accessor :author, :page_count, :genre, :title, :page
   def initialize(title, author=nil, page_count=nil, genre=nil)
     @title = title
     @author= author
     @page_count= page_count
     @genre= genre
+    @page = 0
   end
   
   def turn_page
-    @page_count += 1
+    @page += 1
   end 
   
 end 
