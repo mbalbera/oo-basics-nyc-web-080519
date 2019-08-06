5b70f65e32912cdac085e7afe89ee17494cdba76
Mason Balbera <mhbalbera@gmail.com>
2019-08-06 15:12:52 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
 
 
+class Book 
+  attr_accessor :author, :page_count, :genre, :title, :page
+  def initialize(title, author=nil, page_count=nil, genre=nil)
+    @title = title
+    @author= author
+    @page_count= page_count
+    @genre= genre
+    @page = 0
+  end
+  
+  def turn_page
+    @page += 1
+    puts "Flipping the page...wow, you read fast!"
+  end 
+  
+end 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+
+class Shoe 
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(brand, color=nil, size=nil, material=nil, condition=nil)
+    @brand = brand
+    @color=color
+    @size=size
+    @material=material
+    @condition=condition
+  end
+  
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+  
+end 
