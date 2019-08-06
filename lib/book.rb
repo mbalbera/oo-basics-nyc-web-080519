8ebce0b7322a363a2d5b4840c2e8c6da45f50ae0
Mason Balbera <mhbalbera@gmail.com>
2019-08-06 15:09:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 
 
 class Book 
-  attr_accessor :author, :pages, :genre, :title
+  attr_accessor :author, :page_count, :genre, :title
   def initialize(title, author=nil, page_count=nil, genre=nil)
     @title = title
     @author= author
